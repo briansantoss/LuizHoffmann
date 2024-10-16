10da92806ac0674233b98e1052c1266b777c9170
Brian Santos <autologon@CSED.LAB>
2024-10-16 09:32:19 -0300

    Criando classe para funcionário

--- BrianSantos/Models/AppDataContext.cs
+++ BrianSantos/Models/AppDataContext.cs
@@ -4,8 +4,9 @@ namespace BrianSantos.Models;
 
 public class AppDataContext : DbContext
 {
+    public DbSet<Funcionario> Funcionarios { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite("Data Sourc=luiz-hoffmann_brian-santos.db");
+        optionsBuilder.UseSqlite("Data Source=luiz-hoffmann_brian-santos.db");
     }
 }
