54167590eaed502756a850938d3bba75fb81513a
Brian Santos <autologon@CSED.LAB>
2024-10-16 09:50:30 -0300

    Atualizando arquivos

--- BrianSantos/Program.cs
+++ BrianSantos/Program.cs
@@ -1,6 +1,17 @@
+using BrianSantos.Models;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/funcionario/cadastrar", ([FromBody] Funcionario funcionarioNovo, [FromServices] AppDataContext contexto) =>
+{
+    contexto.Funcionarios.Add(funcionarioNovo);
+    contexto.SaveChanges();
+    return Results.Created("", funcionarioNovo);
+});
 
 app.Run();
